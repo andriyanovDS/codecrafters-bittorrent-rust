ad52fc8003c658adec2d948a7e57aeec29975b03
dandriyanov <dandriyanov@intermedia.com>
2023-10-28 23:52:02 +0400

    Decode bencode string

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,31 @@
 use serde_json;
-use std::env;
+use std::{env, process::exit};
 
 // Available if you need it!
 // use serde_bencode
 
-#[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
-    }
+    encoded_value
+        .split_once(":")
+        .and_then(|(length, string)| {
+            let length = length.parse::<usize>().ok()?;
+            Some(serde_json::Value::String(string[0..length].to_string()))
+        })
+        .expect("Unexpected type {encoded_value:?}")
 }
-
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: decode <bencode string>");
+        exit(1);
+    }
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }

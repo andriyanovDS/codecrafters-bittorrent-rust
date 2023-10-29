4bc23152d7ec3ff1e46a82fe8f87be0b9417e18e
dandriyanov <dandriyanov@intermedia.com>
2023-10-29 16:51:57 +0400

    Parse torrent file info

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,52 @@
 use anyhow::{Error, Result};
+use clap::{Parser, Subcommand};
+use serde::de::Visitor;
+use serde::{Deserialize, Deserializer};
 use serde_json::{Map, Number, Value};
-use std::{env, process::exit};
+use std::fmt::{self, Display};
+use std::marker::PhantomData;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Decode { encoded_value: String },
+    Info { file_path: PathBuf },
+}
+
+#[derive(Deserialize)]
+struct TorrentFile {
+    announce: String,
+    info: Info,
+}
+
+const PIECE_LEN: usize = 20;
+
+#[derive(Debug)]
+struct Piece([u8; PIECE_LEN]);
+
+#[derive(Deserialize)]
+struct Info {
+    length: usize,
+    name: String,
+    #[serde(rename = "piece length")]
+    piece_length: usize,
+    #[serde(deserialize_with = "deserialize_piece")]
+    pieces: Vec<Piece>,
+}
+
+impl Display for TorrentFile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Tracker URL: {}", self.announce)?;
+        writeln!(f, "Length: {}", self.info.length)?;
+        Ok(())
+    }
+}
 
 fn decode_bencoded_value(encoded_value: &str) -> Result<(&str, Value)> {
     let Some(first_char) = encoded_value.chars().next() else {
@@ -59,21 +105,60 @@ fn decode_bencoded_value(encoded_value: &str) -> Result<(&str, Value)> {
         ))),
     }
 }
+
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Usage: decode <bencode string>");
-        exit(1);
-    }
-    let command = &args[1];
-
-    if command == "decode" {
-        let encoded_value = &args[2];
-        let (_, decoded_value) = decode_bencoded_value(encoded_value)?;
-        println!("{}", decoded_value);
-    } else {
-        println!("unknown command: {}", args[1])
+    let cli = Cli::parse();
+    match &cli.command {
+        Command::Decode { encoded_value } => {
+            let (_, decoded_value) = decode_bencoded_value(encoded_value)?;
+            println!("{}", decoded_value);
+        }
+        Command::Info { file_path } => {
+            let file = std::fs::read(file_path)?;
+            let torrent = serde_bencode::from_bytes::<TorrentFile>(&file)?;
+            println!("{torrent}");
+        }
     }
     Ok(())
+}
+
+fn deserialize_piece<'de, D>(deserializer: D) -> Result<Vec<Piece>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    struct PieceVisitor(PhantomData<fn() -> Vec<Piece>>);
+
+    impl<'de> Visitor<'de> for PieceVisitor {
+        /// Return type of this visitor. This visitor computes the max of a
+        /// sequence of values of type T, so the type of the maximum is T.
+        type Value = Vec<Piece>;
+
+        fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+            formatter.write_str("a nonempty sequence of numbers")
+        }
+
+        fn visit_bytes<E>(self, v: &[u8]) -> std::result::Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            let mut pieces = Vec::new();
+            let mut start = 0;
+            while v.len() - start >= PIECE_LEN {
+                let mut buffer = [0u8; PIECE_LEN];
+                v[start..start + PIECE_LEN]
+                    .iter()
+                    .enumerate()
+                    .for_each(|(index, byte)| {
+                        buffer[index] = *byte;
+                    });
+                pieces.push(Piece(buffer));
+                start += PIECE_LEN;
+            }
+            Ok(pieces)
+        }
+    }
+
+    let visitor = PieceVisitor(PhantomData);
+    deserializer.deserialize_seq(visitor)
 }
